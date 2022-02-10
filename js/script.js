1b6e7886ab0e38a524d720a2227b520d8c90cd2f
JoelmaRegina <joelma_silvaa@hotmail.com>
2022-02-09 23:46:07 -0300

    Added more CSS codes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+  });
+
 function showDescription (){
     document.getElementById("prato-main").onmouseover().getElementById(".link-main").style.display("flex")
 }
